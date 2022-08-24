201fbd3e530012eaa893f6b06d9672b0751c53de
Zhanna Doronina <doronina.jane@mail.ru>
2022-08-24 14:55:00 +0100

    implemented all user journeys no styling no refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+chishuru = {name: "Chishuru", address: "Unit 9 Market Row, Coldharbour Lane", category: "chinese" }
+manteca = {name: "Manteca", address: "49-51 Curtain Road", category: "italian" }
+bibi = {name: "Bibi", address: "42 North Audley St", category: "japanese" }
+planque = {name: "Planque", address: "322-324 Acton Mews", category: "french" }
+sichuan = {name: "Sichuan Folk", address: "14 City Rd", category: "chinese" }
+
+[chishuru, manteca, bibi, planque, sichuan].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+end
